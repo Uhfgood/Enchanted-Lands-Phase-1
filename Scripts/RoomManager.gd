436f2ff4130222fe87a92be980bf9f6fb5d773fc
Uhfgood <Uhfgood@hotmail.com>
2025-01-10 11:54:15 -0500

    Update RoomManager.gd
    removed superfluous code and comments

--- Scripts/RoomManager.gd
+++ Scripts/RoomManager.gd
@@ -1,7 +1,6 @@
 extends Node
 
 @onready var room_viewer: Node = $RoomViewer
-#@onready var main_menu: Room = $MainMenu
 @onready var viewer_text: Node = $RoomViewer/Description/DescText
 
 var curr_room : Room = null
@@ -9,13 +8,6 @@ var curr_room : Room = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	ChangeRoom( "main_menu" )
-
-#func ChangeRoom( new_room : Node ) -> void:
-#	curr_room = new_room
-#	if curr_room and viewer_text:
-#		viewer_text.text = curr_room.description
-#	else:
-#		print( "either curr_room or viewer_text not valid" )
 
 func ChangeRoom( new_room : String ) -> void:
 	
@@ -33,45 +25,6 @@ func ChangeRoom( new_room : String ) -> void:
 	else:
 		print( "either curr_room or viewer_text not valid" )
 
-func FindConnectedRoom( room_name : String ) -> Room:
-	var rooms = self.get_children()
-	for room in rooms:
-		if room is Room:
-			if room.room_name == room_name:
-				return room
-	return null
-	
-# return a room if it's connected to the current room.
-func GetConnectedRoom( room_name : String ) -> Room:	
-	var parent = null
-	if curr_room:
-		parent = curr_room.get_parent()
-			
-	if parent and parent is Room and parent.room_name == room_name:
-		return parent 
-	else:
-		var children = curr_room.get_children()
-		for child in children:
-			if( child is Room ):
-				if( child.room_name == room_name ):
-					return child
-	return null
-	
-func FindRoomWithName( node: Room, room_name : String ) -> Room:
-	if "room_name" in node:
-		if( room_name == node.room_name ):
-			return node
-			
-		for child in node.get_children():
-			var result = FindRoomWithName(child, room_name)
-			if result:
-				return result
-	return null
-
-# return a room anywhere on the tree (starting with MainMenu)
-#func GetRoom(room_name: String) -> Node:
-#	return FindRoomWithName(main_menu, room_name)
-
 func _input( event : InputEvent ) -> void:
 	#var choice : Room = null;
 	var choice_number = 1
@@ -80,12 +33,7 @@ func _input( event : InputEvent ) -> void:
 		var action_name = "choice" + str(choice_number)
 		if event.is_action_pressed( action_name ):
 			if curr_room.choices[ choice_number ] != "":
-				#choice = GetConnectedRoom( curr_room.choices[ choice_number ] )
-				#choice = FindConnectedRoom( curr_room.choices[ choice_number ] )
-				#if choice:
-					#ChangeRoom( choice )
 				ChangeRoom( curr_room.choices[ choice_number ] )
-					
 					
 		choice_number += 1
 		
