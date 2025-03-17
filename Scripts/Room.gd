82796485b5ce18cf2fafce3b3cb60c173af77aa4
Uhfgood <Uhfgood@hotmail.com>
2025-03-17 18:54:37 -0400

    Moved a bunch of stuff around, loading up json in project

--- Scripts/Room.gd
+++ Scripts/Room.gd
@@ -1,3 +1,43 @@
 class_name Room extends Node
 @export var room_name : String = "blank_room"
 @export_multiline var description : String = "Modify the description text to describe your scene, and add your choices.  Make sure to number your choices up to 9, and add 0 for Exit."
+
+func LoadDataFromJSON( json_name : String )->bool:
+	var filename = "res://Rooms/" + json_name + ".json"
+	
+	if not FileAccess.file_exists( filename ):
+		print( filename + " does not exist." )
+		
+	# load the json here:
+	var file = FileAccess.open( filename, FileAccess.READ )
+	if not file:
+		print( "Error loading file from json: " + filename )
+		return false
+		
+	# parse json
+	var json_data = JSON.parse_string( file.get_as_text() )
+	file.close()
+	if json_data == null:
+		print( "Error parsing JSON data: " + filename )
+		return false
+		
+	# set room properties
+	self.room_name = json_data[ "name" ]
+	self.description = json_data[ "description" ]
+	
+	# create and attach door nodes
+	for door_data in json_data[ "doors" ]:
+		var new_door = Door.new()
+		new_door.choice = door_data[ "choice" ]
+		new_door.destination = door_data[ "destination" ]
+		add_child( new_door )
+		
+	return true
+	
+static func CreateRoomFromJSON( json_name : String )->Room:
+	var new_room = Room.new()
+	
+	if new_room.LoadDataFromJSON( json_name ) == false:
+		return null
+				
+	return new_room
--- Scripts/RoomManager.gd
+++ Scripts/RoomManager.gd
@@ -7,29 +7,44 @@ var curr_room : Room = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	curr_room = LoadRoom( "main_menu" )
 	ChangeRoom( "main_menu" )
 
-func LoadRoomDataFromJSON( room_name : String ) -> Room:
+func LoadRoom( room_name : String ) -> Room:
 	var room = null
+	var filename = "res://Scenes/" + room_name + ".tscn";
+	if ResourceLoader.exists( filename ):
+		var scene = load( filename )
+		if scene != null:
+			room = scene.instantiate()
+		else:
+			print( "Couldn't load " + filename + ", trying json." )
+			room = null
+			room = Room.CreateRoomFromJSON( room_name )
+	else:
+		print( "The file: " + filename + " doesn't exist.  Trying json." )
+		room = null
+		room = Room.CreateRoomFromJSON( room_name )
+		
 	return room
 	
 func ChangeRoom( room_name : String ) -> void:
-	var scene = load( "res://Scenes/Rooms/" + room_name + ".tscn" )
-	if scene != null:
-		var new_room = scene.instantiate()
-		if curr_room != null:
-			remove_child( curr_room )
-			curr_room.queue_free()
-			curr_room = null
+	var new_room = LoadRoom( room_name )
+	if curr_room != null:
+		remove_child( curr_room )
+		curr_room.queue_free()
+		curr_room = null
 		
 		if curr_room == null:
 			curr_room = new_room
 			add_child( curr_room )
-			
-		if curr_room and viewer_text:
-			viewer_text.text = curr_room.description
-		else:
-			print( "either curr_room or viewer_text not valid" )
+				
+			if curr_room and viewer_text:
+				viewer_text.text = curr_room.description
+			else:
+				print( "either curr_room or viewer_text not valid" )
+	else:
+		print( room_name + " creation unsuccessful." )		
 
 func _input( event : InputEvent ) -> void:
 	var choice_number = 1
