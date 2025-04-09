31400a05342ff2a1e902d2e878a3de83abb9fdc1
Uhfgood <Uhfgood@hotmail.com>
2025-04-09 11:51:52 -0400

    removed tscn loading in the editor map

--- Scripts/editor_map.gd
+++ Scripts/editor_map.gd
@@ -1,9 +1,6 @@
 @tool
 extends Node2D
 
-# Custom signal to initialize rooms
-#signal initialize_room()
-
 # Flag to prevent load_all_rooms from running multiple times
 var has_loaded_rooms: bool = false
 
@@ -14,8 +11,6 @@ func _ready():
 		if not has_loaded_rooms:
 			load_all_rooms()
 			has_loaded_rooms = true
-			#print( "***" )
-
 
 func load_all_rooms():
 	print("Running load_all_rooms")
@@ -27,39 +22,6 @@ func load_all_rooms():
 	var rooms_dict = {}  # Maps room name (e.g., "sn000-MainMenu") to Room node
 	var file_list = []
 	var file_name = ""
-
-	# Load .tscn files
-	var tscn_dir = DirAccess.open("res://Rooms/")
-	if tscn_dir:
-		file_list = tscn_dir.get_files()
-		for item in file_list:
-			file_name = item
-			if file_name.ends_with(".tscn"):
-				print("---")
-				print( "Next tscn in file list: ", item)
-				var scene_path = "res://Rooms/" + file_name
-				var room = load(scene_path).instantiate()
-				var room_name = room.name
-				print( "room_name = ", room_name )
-				rooms_dict[room_name] = room
-				add_child(room)
-				room.owner = get_tree().edited_scene_root
-				room.editor_map = self
-				
-				for child in room.get_children():
-					if child is Door:
-						# Create a new Door instance with a unique name
-						var editor_door = Door.new()
-						editor_door.door_id = child.door_id
-						editor_door.choice = child.choice
-						editor_door.destination = child.destination
-						editor_door.name = "em_" + child.name
-						room.doors.append(editor_door)
-						room.add_child(editor_door)
-						editor_door.owner = get_tree().edited_scene_root
-						
-						
-				room.SetupVisuals()  # Direct call
 
 	# Load .json files
 	file_list = []
